3d1160912c0f4584c312bf2c6569a5e939802d02
Youhei SASAKI <uwabami@gfd-dennou.org>
2011-04-30 16:54:34 +0900

    create template and change html -> xhtml
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

--- muse-config.el
+++ muse-config.el
@@ -5,48 +5,18 @@
 (require 'muse-html)
 (require 'muse-project)
 
+(muse-derive-style "ksp" "xhtml"
+                   :author "ksp-ja <ksp-ja_(at)_googlegroups.com>"
+                   :style-sheet "style.css"
+                   :header "template/header.html"
+                   :footer "template/footer.html"
+                   )
 (setq muse-project-alist
-      `(("ksp"			
-	 (,default-directory :default "index")
-	 (:base "html" :path ,(concat default-directory "html")))))
-(setq muse-html-style-sheet "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">")
+      `(("ksp"
+         (,default-directory :default "index")
+         (:base "ksp" :path ,(concat default-directory "html")))))
 
-(setq muse-html-header
-      (concat 
-"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">
-<html>
-  <head>
-    <title><lisp>
-  (concat (muse-publishing-directive \"title\")
-          (let ((author (muse-publishing-directive \"author\")))
-            (if (not (string= author (user-full-name)))
-                (concat \" (by \" author \")\"))))</lisp></title>
-    <meta name=\"generator\" content=\"muse.el\">
-    <meta http-equiv=\"<lisp>muse-html-meta-http-equiv</lisp>\"
-          content=\"<lisp>muse-html-meta-content-type</lisp>\">
-    <lisp>
-      (let ((maintainer (muse-style-element :maintainer)))
-        (when maintainer
-          (concat \"<link rev=\\\"made\\\" href=\\\"\" maintainer \"\\\">\")))
-    </lisp>
-    <lisp>muse-html-style-sheet</lisp>
-  </head>
-  <body>
-    <h1><lisp>
-  (concat (muse-publishing-directive \"title\")
-          (let ((author (muse-publishing-directive \"author\")))
-            (if (not (string= author (user-full-name)))
-                (concat \" (by \" author \")\"))))</lisp></h1>
-    <div class=\"bodydiv\">
-    <!-- Page published by Emacs Muse begins here -->
-"))
-
-(setq muse-html-footer (concat 
-"<!-- Page published by Emacs Muse ends here -->
-    </div>
-    <div class=\"toplinks\">
-      <a class=\"toplinks\" href=\"index.html\">Index</a><br>
-    </div>    
-
-  </body>
-</html>\n"))
+(require 'time-stamp)
+(defun muse-html-time-stamp()
+  (setq time-stamp-format "%:y/%:m/%d %02H:%02M")
+  (concat "Last Update: " (time-stamp-string)))
